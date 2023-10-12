65a1911654509861deee29c7ef165af907fa1b05
jermb <77866654+jermb@users.noreply.github.com>
2023-10-11 21:30:57 -0500

    Webscraping done (tentatively)
    course.data.json and geneds-reqs.json are most relevant files

--- index.js
+++ index.js
@@ -2,39 +2,37 @@ const puppeteer = require("puppeteer");
 
 (async () => {
   const courses = {};
-  const browser = await puppeteer.launch(); // Starts virtual browser
-  const page = await browser.newPage(); // Opens a tab in the browser
-  await page.goto("https://catalog.missouri.edu/courseofferings/"); // Navigates to course offering page
+  const browser = await puppeteer.launch();                                                         // Starts virtual browser
+  const page = await browser.newPage();                                                             // Opens a tab in the browser
+  await page.goto("https://catalog.missouri.edu/courseofferings/");                                 // Navigates to course offering page
  
-  links = await page.evaluate(() => {
-    links = document.querySelectorAll("#co_departments ul li a"); // Gets the link to every page of courses
-    links_array = []
+  const links = await page.evaluate(() => {
+    const links = document.querySelectorAll("#co_departments ul li a");                             // Gets the link to every page of courses
+    const links_array = []
 
     links.forEach(link => {
-      links_array.push(link.href);  // Gets the actual link to the page
+      links_array.push(link.href);                                                                  // Gets the actual link to the page
     })
     return links_array;
   });
 
   // links.splice(1); // used in testing for faster running
 
-  for (var link of links) { // Go through every course list
-    await page.goto(link);  // Navigate to the page
-    course_array = await page.evaluate(() => {
-      headings_elements = document.querySelectorAll(".courseblock");  // Grabs the div for each course
-      headings_array = Array.from(headings_elements); 
+  for (var link of links) {                                                                         // Go through every course list
+    await page.goto(link);                                                                          // Navigate to the page
+    const course_info = await page.evaluate(() => {
+      const h1 = document.querySelector("#content h1")?.textContent;
+      const rows = document.querySelectorAll(".courseblock");                                       // Grabs the div for each course
 
-      course_array = [] // Empty array to house course objects
+      const course_array = []                                                                       // Empty array to house course objects
 
-      headings_array.forEach(block => {
-        const title_line = block.querySelector(".courseblocktitle").textContent.split(":  "); // The title has both the course # and name, so we split them 
-        const number = title_line[0];
-        const title = title_line[1];
-        title.replace('\u00a0', " "); // Removes non-breaking spaces which are used in ALL course ids
+      rows.forEach(row => {
+        const title_line = row.querySelector(".courseblocktitle").textContent.split(":  ");         // The title has both the course # and name, so we split them 
+        const number = title_line[0].replace(/\u00a0/g, "_");
+        const title = title_line[1];                                                                // Removes non-breaking spaces which are used in ALL course ids
 
-        const text = block.querySelector(".courseblockdesc").textContent;
-        text.replace('\u00a0', " ");  //  Non-breaking spaces again
-        const regex = /Credit Hours: (\d+).*?Prerequisites: (.+?)\n/; // Splits the body of the courseblock into 3 parts, the description, the credit hours, and the prereqs
+        const text = row.querySelector(".courseblockdesc").textContent?.replace(/\u00a0/g, " ");    // Non-breaking spaces again
+        const regex = /Credit Hour(?:s)?: ((?:\d+-\d+|\d+))(?:.*?Prerequisites: (.*?))?\n/;         // Splits the body of the courseblock into 3 parts, the description, the credit hours, and the prereqs
         const matches = text.match(regex);
 
         //  Prevent emptys
@@ -48,18 +46,16 @@ const puppeteer = require("puppeteer");
   
           textBeforeMatch = text.slice(0, text.indexOf(matches[0])).trim();
         }
-
-
         // Creates an object for each course
-        course_array.push({number: number, title: title, credit_hours: creditHours, prerequisites: prerequisites, description: textBeforeMatch});
+        course_array.push({ number: number, title: title, credit_hours: creditHours, prerequisites: prerequisites, description: textBeforeMatch });
       });      
 
-      return course_array;
+      return { courses: course_array, program: h1 };
     });
-    courses[link] = course_array; // creates key value pair of link with courses. should probably replace with a better major identifier.
+    courses[course_info.program] = course_info.courses;                 // creates key value pair of link with courses. should probably replace with a better major identifier.
 
   }
-  await browser.close();  // Closes browser, womp womp
+  await browser.close();                                                // Closes browser, womp womp
 
   //  Turn to json and put into document
   var jsonData = JSON.stringify(courses);
---
+++ join.js
@@ -0,0 +1,40 @@
+const courses = require('./courses.json');
+const years = require('./years.json');
+const geneds = require('./geneds.json');
+const reqs = require('./geneds-reqs.json');
+
+// This file is just to combine info from several pages into something closer to what the database will be.
+
+const new_courses = {};
+
+for (let [key, vals] of Object.entries(courses)) {
+    const new_vals = []
+    vals.forEach((course) => {
+        const nc = {};
+        
+        // I only did it this v way because i wanted it be in a nice order LOL
+
+        nc["course_id"] = course.number;           
+        nc["title"] = course.title;
+        nc["credit_hours"] = course.credit_hours;
+        nc["prerequisites"] = course.prerequisites;
+        nc["description"] = course.title;
+        nc["category"] = geneds[course.number]?.category;
+        nc["properties"] = geneds[course.number]?.properties;
+        nc["past_terms_offered"] = years[course.number];
+        new_vals.push(nc);
+
+    });
+    new_courses[key] = new_vals;
+
+}
+
+
+
+var jsonData = JSON.stringify(new_courses);
+var fs = require('fs');
+fs.writeFile("course.data.json", jsonData, function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
