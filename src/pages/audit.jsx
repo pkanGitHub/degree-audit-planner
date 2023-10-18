5f0c766b3b9b4f412d1012a7d20dfc8c35614bd0
flc3v9 <97908586+flc3v9@users.noreply.github.com>
2023-10-18 14:15:18 -0500

    class design

--- src/pages/audit.jsx
+++ src/pages/audit.jsx
@@ -80,6 +80,41 @@ const Audit = () => {
                        
                     </div>
 
+                    <div id="choiceCourse">
+                        <table>
+                            <tr>
+                                <th>BIOME 1500</th>
+                                <th id="removeItem"><button id="removeButton">X</button></th>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Credits: 5
+                                </td>
+                                <td>
+                                <select className="requiredCourseSelect">
+                                    <option value='taken'>Taken</option>
+                                    <option value='IP'>In Progress</option>
+                                    <option value='planned'>Planned</option>
+                                </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Prerequisites: N/A
+                                </td>
+                                <td>
+                                    <select className="requiredCourseSelect">
+                                        <option value='FS2021'>FS2021</option>
+                                        <option value='SP2022'>SP2022</option>
+                                        <option value='SM2022'>SM2022</option>
+                                        <option value='FS2022'>FS2022</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        </table>
+                       
+                    </div>
+
                 </div>
             </div>
         </div>
