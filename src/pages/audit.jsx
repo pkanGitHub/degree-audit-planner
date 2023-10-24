5ae04d081319912f364580a13f55182166b8ba4b
flc3v9 <97908586+flc3v9@users.noreply.github.com>
2023-10-24 14:37:21 -0500

    class select based on input

--- src/pages/audit.jsx
+++ src/pages/audit.jsx
@@ -2,7 +2,7 @@ import "../styles/audit.css";
 import RequiredChoice from "../components/requiredChoice";
 import RequiredCourse from "../components/requiredCourse";
 import ClassInfo from "../components/classInfoPopup";
-import { useState, useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 
 const Audit = () => {
     const [selectCourseType, setCourseType] = useState('');
@@ -17,82 +17,58 @@ const Audit = () => {
     const courseNumber = [1400, 1500, 2000, 2050]
 
 
-  const [selectType, setType] = useState("");
-  const typeOptions = ["Major", "Minor", "Certificate"];
-  const handleTypeChange = (e) => {
-    setType(e.target.value);
-  };
+    const [selectType, setType] = useState("");
+    const typeOptions = ["Major", "Minor", "Certificate"];
+    const handleTypeChange = (e) => {
+        setType(e.target.value);
+    };
 
-  const [selectCategory, setCategory] = useState("");
-  const category = {
-    INFOTC: "Information Technology - BS",
-    BIOMED: "Biomedical Engineering - BMEBS",
-    COMPS: "Computer Science - BS",
-  };
-  const handleCategoryChange = (e) => {
-    setCategory(e.target.value);
-  };
+    const [selectCategory, setCategory] = useState("");
+    const category = {
+        INFOTC: "Information Technology - BS",
+        BIOMED: "Biomedical Engineering - BMEBS",
+        COMPS: "Computer Science - BS",
+    };
+    const handleCategoryChange = (e) => {
+        setCategory(e.target.value);
+    };
 
-  const [selectTerm, setTerm] = useState("");
-  const term = {
-    FS21: "Fall 2021",
-    SP22: "Spring 2022",
-    SM22: "Summer 2022",
-  };
-  const handleTermChange = (e) => {
-    setTerm(e.target.value);
-  };
+    const [selectTerm, setTerm] = useState("");
+    const term = {
+        FS21: "Fall 2021",
+        SP22: "Spring 2022",
+        SM22: "Summer 2022",
+    };
+    const handleTermChange = (e) => {
+        setTerm(e.target.value);
+    };
 
-  //dynamic categories
-  const categories = {MATH: "Math", ENG: "English"}
+    //dynamic categories
+    const categories = {MATH: "Math", ENG: "English"}
 
-  //dynamic required courses
-  const requiredCourseData = [{classId: "MATH1500", creditHours: 5, preReq: "N/A"}, {classId: "MATH2500", creditHours: 5, preReq: "MATH2500"}, {classId: "ENG3000", creditHours: 3, preReq: "ENG2000"}]
+    //dynamic required courses
+    const requiredCourseData = [{classId: "MATH1500", creditHours: 5, preReq: "N/A"}, {classId: "MATH2500", creditHours: 5, preReq: "MATH2500"}, {classId: "ENG3000", creditHours: 3, preReq: "ENG2000"}]
 
-  //dynamic course choice
-  const classData = [{classId: "MATH1500", creditHours: 5, preReq: "N/A"}, {classId: "BME1000", creditHours: 3, preReq: "PHYSICS1000"}]
+    //dynamic course choice
+    const classData = [{classId: "MATH1500", creditHours: 5, preReq: "N/A"}, {classId: "BME1000", creditHours: 3, preReq: "PHYSICS1000"}]
 
-  //delete entry
-  //https://stackoverflow.com/questions/61661526/react-delete-one-children-without-rendering-the-parent-again
+    //delete entry
+    //https://stackoverflow.com/questions/61661526/react-delete-one-children-without-rendering-the-parent-again
 
-  //class type select dynamic
+    //class type select dynamic
 
-  const classSelect = {BIO: "Biology", AGR: "Agriculture", INT: "International Studies", MECHE: "Mechanical Engineering"}
+    const classSelect = {"INFOTC": "Information Technology", "ENGINR": "Engineering", "CMP_SC": "Computer Science", "MAE": "Mechanical Engineering"}
 
-    const popupClasses = [{className: "INFOTC 1000", classTitle: " Introduction to Information Technology", classDescript: "Introduction to Information Technology introduces the field of Information Technology including foundation experiences and knowledge, the history of digital technologies, emphasis areas in the  program, software engineering, computer networks and the internet, web development, current trends  in technology, career opportunities, and ethical/social issues. Students participate in activities  that introduce students to digital media, digital systems, and software engineering. Students learn to use distributed version control systems and how to work on collaborative teams.", creditHours: "3", preReq: "N/A", lastOffered: "Fall 2023"}, {className: "ENGINR 2011", classTitle: "Engineering Leadership and Strategic Communication", classDescript: "This course is inspired by the experience and writings of CEO and world-renowned leader David Novak. It is designed to introduce engineering students to the concepts, theory, and practice of engineering leadership. Topics include; effective written and oral communications, presentations, engineering leadership characteristics, individual differences and self-awareness, and developing and building teams. Graded on A-F basis only.", creditHours: "3", preReq: "N/A", lastOffered: "Fall 2023"}];
+    const popupClasses = [{className: "INFOTC 1000", classTitle: "Introduction to Information Technology", classDescript: "Introduction to Information Technology introduces the field of Information Technology including foundation experiences and knowledge, the history of digital technologies, emphasis areas in the  program, software engineering, computer networks and the internet, web development, current trends  in technology, career opportunities, and ethical/social issues. Students participate in activities  that introduce students to digital media, digital systems, and software engineering. Students learn to use distributed version control systems and how to work on collaborative teams.", creditHours: "3", preReq: "N/A", lastOffered: "Fall 2023"}, {className: "ENGINR 2011", classTitle: "Engineering Leadership and Strategic Communication", classDescript: "This course is inspired by the experience and writings of CEO and world-renowned leader David Novak. It is designed to introduce engineering students to the concepts, theory, and practice of engineering leadership. Topics include; effective written and oral communications, presentations, engineering leadership characteristics, individual differences and self-awareness, and developing and building teams. Graded on A-F basis only.", creditHours: "3", preReq: "N/A", lastOffered: "Fall 2023"}, {className: "CMP_SC 2270", classTitle: "Introduction to Logic Systems", classDescript: "(same as ECE 2210). Basic tools, methods and procedures to design combinational and sequential digital circuits and systems, including number systems, boolean algebra, logic minimization, circuit design, memory elements, and finite state machine design.", creditHours: "3", preReq: "C or higher in CMP_SC 1050 or INFOTC 1040", lastOffered: "Summer 2021"}, {className: "MAE 2200", classTitle: "Engineering Materials", classDescript: "The nature of the structure of engineering materials. The relationship of material structure to physical properties. Mechanical behavior of engineering materials. Graded on A-F basis only.", creditHours: "3", preReq: "Grade of C- or better in ENGINR 1200 and CHEM 1320. Restricted to Mechanical and Aerospace Engineering students only", lastOffered: "Spring 2022"}];
+
 
     // testing add function! this is applied to the mass select, will need to get this to work on the top and other select course
-    const inputArr = [
-        {
-          id: 1,
-          value: ""
-        }
-    ];
 
-    const [arr, setArr] = useState(inputArr);
-
-    const addInput = () => {
-    setArr(s => {
-        return [
-        ...s,
-        {
-            value: ""
-        }
-        ];
-    });
-    };
-
-    const handleAddChange = e => {
-    e.preventDefault();
-
-    const index = e.target.id;
-    setArr(s => {
-        const newArr = s.slice();
-        newArr[index].value = e.target.value;
-
-        return newArr;
-    });
-    };
+    const [largeClassSelectFields, setLargeFields] = useState([{courseType: "", courseNumber: ""}]) // declares use state that is multiple dictionaries in an array
+    const addLargeClassSelectField = () => {
+        let newField = {courseType: "", courseNumber: ""} // declares new field as a course type
+        setLargeFields([...largeClassSelectFields, newField]) // appends new dictionary field to the array
+    }
 
     const [enrollFields, setEnrollFields] = useState([{type: "", category: "", year: ""}])
     const addEnrollFields = () => {
@@ -192,47 +168,53 @@ const Audit = () => {
                             <label id='genReqLabel' htmlFor="third">Sample large class select</label>
                             <div className="classHistory">
                                 <div id='largeClassSelect'>
-                                    <label>
-                                        Course type:
-                                        <select id='chooseCourseType' onChange={handleSelect}>
-                                            {Object.keys(classSelect).map((key, index) => 
-                                            <option value={key}>{classSelect[key]}</option>)}
-                                        </select>
-                                    </label>
-                                    {/* will want to find a way to pass information through to the pop up, will have to use query based on selection above*/}
-                                    {/* https://www.telerik.com/blogs/how-to-programmatically-add-input-fields-react-forms might want this later*/}
-                                    <p>
-                                        Choose from {selectCourseType} courses below:
-                                    </p>
-
                                     
-                                    <div id="popupDiv">
-                                        {popupClasses.map((key, index) => <ClassInfo key={index} className={key.className} classTitle={key.classTitle} classDescript={key.classDescript} creditHours={key.creditHours} preReq={key.preReq} lastOffered={key.lastOffered}/>)}
-                                    </div>
-
-                            
                                     {/* Need to make this where you get the information from thee choose course type and course number and then you submit this to the requiredChoice component */}
-                        
-                                    <div>
-                                        {/* right now, this only generates the add course property, will want this for the whole section */}
-                                        {arr.map((item, i) => {
-                                            return (
+                                    
+                                    {largeClassSelectFields.map((input, index) => {
+                                        return(
+                                            <div key={index} id="addedLargeSelection">
+                                                <label>
+                                                    Course type:
+                                                    <select id='chooseCourseType' name="courseType" onChange={handleSelect}>
+                                                        {Object.keys(classSelect).map((key, index) => 
+                                                        <option value={key}>{classSelect[key]}</option>)}
+                                                    </select>
+                                                </label>
+
+                                                {/* will want to find a way to pass information through to the pop up, will have to use query based on selection above*/}
+                                                {/* https://www.telerik.com/blogs/how-to-programmatically-add-input-fields-react-forms might want this later*/}
+                                                <p>
+                                                    Choose from {selectCourseType} courses below:
+                                                </p>
+
+                                                {popupClasses.filter(item => {
+                                                    if (!selectCourseType) return false
+                                                    if (item.className.includes(selectCourseType)) {
+                                                        return true
+                                                    }
+                                                }).map(item => (
+                                                    <div id="popupDiv">
+                                                        <ClassInfo key={item} className={item.className} classTitle={item.classTitle} classDescript={item.classDescript} creditHours={item.creditHours} preReq={item.preReq} lastOffered={item.lastOffered}/>
+                                                    </div>
+                                                ))
+                                                }
+
+                                                
+                                
+
                                                 <label>
                                                     Course Number: 
-                                                    <select
-                                                        onChange={handleAddChange}
-                                                        value={item.value}
-                                                        id={i}
-                                                    >
+                                                    <select name="courseNumber">
                                                         {courseNumber.map((courseNumber) => (
                                                         <option value={courseNumber}>{courseNumber}</option>))}
                                                     </select>
                                                 </label>
-                                            );
-                                        })}
-                    
-                                        <button onClick={addInput} id='addCourseButton'>Add Course</button>
-                                    </div>
+                                            </div>
+                                        )
+                                    })}
+                                    <button onClick={addLargeClassSelectField} id='addCourseButton'>Add Course</button>
+                                    
                                 </div>
                             </div>
                         </li>
