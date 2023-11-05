6624b958097879506ce7a60eefc588be54ea15d1
Phyllis Kan <pyk9q7@gmail.com>
2023-11-05 12:33:43 -0600

    comment other fields for user sign up

--- backend/Models/user.js
+++ backend/Models/user.js
@@ -3,17 +3,17 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    major: { type: String, required: true },
-    coursePlan: {
-        semester: [{
-            date: { type: Date, required: true },
-            courses: [{
-                inProgress: { type: Boolean, default: false },
-                completed: { type: Boolean, required: true },
-                completion_date: { type: Date }
-            }]
-        }]
-    }
+    // major: { type: String, required: true },
+    // coursePlan: {
+    //     semester: [{
+    //         date: { type: Date, required: true },
+    //         courses: [{
+    //             inProgress: { type: Boolean, default: false },
+    //             completed: { type: Boolean, required: true },
+    //             completion_date: { type: Date }
+    //         }]
+    //     }]
+    // }
 })
 
 module.exports = mongoose.model('User', userSchema)
