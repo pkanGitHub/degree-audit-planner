f39ce0cb4168d53c96f9814e51f13deff799670f
Phyllis Kan <pyk9q7@gmail.com>
2023-11-12 10:55:16 -0600

    finish login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,25 +1,61 @@
+import React, {useState} from 'react'
 import "../styles/formStyle.css"
 
-
 const Login = () => {
+    const [formData, setFormData] = useState({ email: '', password: '' })
+    const [errorMsg, setErrorMsg] = useState(null)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+            const response = await fetch('http://localhost:4000/login', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(formData),
+            });
+        
+            const data = await response.json();
+        
+            if (response.ok) {
+              console.log('Login successful on the frontend');
+              // Redirect or update UI...
+                window.location.href = '/audit';
+            } else {
+                setErrorMsg(data.message)
+              console.log('Login failed on the frontend:', data.message);
+              // Update UI or show error message...
+            }
+          } catch (error) {
+            console.error('Error during login on the frontend:', error);
+        }
+    }
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value })
+        setErrorMsg(null)
+      };
+
     return (
         <div className="formSection">
+            {errorMsg && <p style={{ color: 'red' }}>{errorMsg}</p>}
             <div id="formDesign">
                 <h1>Login</h1>
-                <form>
-                    
+                <form onSubmit={handleSubmit}>
                     <div id="formContent">
                         <label >Email</label>          
-                        <input type="text" name="loginemail" placeholder="Enter email here..."/>
+                        <input type="text" name="email" placeholder="Enter email here..." value={formData.email} onChange={handleChange}/>
                         <br/>
                         <label>Password</label>
-                        <input type="text" name="loginemail" placeholder="Enter password here..."/>
+                        <input type="text" name="password" placeholder="Enter password here..." value={formData.password} onChange={handleChange}/>
         
                         <a href="/forgotpassword">Forgot Password?</a>
                     </div>
                     
                     <br/>
-                    <button className="submitButton">Login</button>
+                    <button type="submit" className="submitButton">Login</button>
                     <br/>
                     <a href="/signup">Don't have an account? Sign up here!</a>
 
