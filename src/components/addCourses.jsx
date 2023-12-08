93abd3e78b3b76658e1c90454ef89e514a645d5f
jermb <77866654+jermb@users.noreply.github.com>
2023-12-07 18:20:41 -0600

    the search now works

--- src/components/addCourses.jsx
+++ src/components/addCourses.jsx
@@ -111,7 +111,7 @@ const AddCourses = ({ courses, orCourses, update, userCourses }) => {
 
 
             <div id="popupDiv">
-                {sortedTotalCourses.filter(course => course.name?.match(searchFilter, "gi") || course.classId?.match(searchFilter, "gi")).map((singleCourse, index) => {
+                {sortedTotalCourses.filter(course => course.name?.match(new RegExp(searchFilter, "gi")) || course.classId?.match(new RegExp(searchFilter, "gi"))).map((singleCourse, index) => {
                     if ((orCourses.filter(course => course.orId.match(singleCourse.id))).length > 0) {
                         return (
                             <div key={singleCourse.id}>
